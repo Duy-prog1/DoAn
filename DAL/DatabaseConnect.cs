93076a2fad292d5246a223b3f69ebbec3744cefd
Tran Truong <chantruong753@gmail.com>
2023-10-31 16:16:21 +0700

    fix xong 99%

--- DAL/DatabaseConnect.cs
+++ DAL/DatabaseConnect.cs
@@ -14,35 +14,12 @@ namespace DAL
     {
         private SqlDataAdapter dataTable;
         //ông nào muốn chạy thì mở phần comment của mình, không xóa các comment connect chung
-       protected SqlConnection _conn = new SqlConnection("Data Source=LAPTOP-1A0D861M\\TRANQUANGTRUONG;Initial Catalog=cSharp;Integrated Security=True");
-       // protected SqlConnection _conn = new SqlConnection("Data Source=DESKTOP-87V13PA\\SQLEXPRESS;Initial Catalog=cSharp;Integrated Security=True");
+        protected SqlConnection _conn = new SqlConnection("Data Source=LAPTOP-1A0D861M\\TRANQUANGTRUONG;Initial Catalog=cSharp;Integrated Security=True");
+        // protected SqlConnection _conn = new SqlConnection("Data Source=DESKTOP-87V13PA\\SQLEXPRESS;Initial Catalog=cSharp;Integrated Security=True");
 
 
-        //phần học tại lớp
-        String strConnect = @"Data Source=LAPTOP-1A0D861M\TRANQUANGTRUONG;Initial Catalog=cSharp1;Integrated Security=True";
-        protected bool OpenDB()
-        {
-            if(_conn.State==ConnectionState.Open)
-            {
-                return true;
-            }
-            else
-            { 
-                return false; 
-            }
-        }
-        DatabaseConnect databaseConnect = new DatabaseConnect();
-        DataTable dt;
-        public List<NhanVienDAL> getAll()
-        {
-            dt=new DataTable();
-            List <NhanVienDAL> ls= new List<NhanVienDAL>();
-            string sql = "select * from NhanVien";
-            if (!databaseConnect.OpenDB()) return null;
-            
-
-            return ls;
-        }
         
+       
+
     }
 }
